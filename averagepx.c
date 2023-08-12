005dc1763837189face5aac938f4069546207e69
VorakornK <vorakorn.ko@ku.th>
2023-08-12 22:08:38 +0700

    add file

---
+++ averagepx.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int N, num[N];
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &num[i]);
+    }
+
+    int sum = 0;
+    int num_threads = omp_get_max_threads();
+    int local_sums[num_threads];
+
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        local_sums[thread_id] = 0;
+
+        for (int i = thread_id; i < N; i += num_threads) {
+            local_sums[thread_id] += num[i];
+        }
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        sum += local_sums[i];
+    }
+
+    int average = sum / N;
+    printf("%d\n", average);
+
+    return 0;
+}
